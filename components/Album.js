ccd8a1d6d23ae98f305df9b4550cb4dd55ccaba0
Jeff Yue <jeffyue100@gmail.com>
2023-12-02 18:35:14 -0800

    Created front end for album ratings

--- components/Album.js
+++ components/Album.js
@@ -1,35 +1,44 @@
 import { View, Text, Image, StyleSheet } from "react-native";
 import React from "react";
+import RatingDropdown from "./RatingDropdown";
 
 const Album = ({ name, image, artist }) => {
-  return (
-    <View style={styles.container}>
-      <Image style={styles.albumImage} source={{ uri: image }} />
-      <View style={styles.text}>
-        <Text>{name}</Text>
-        <Text style={styles.artist}>{artist}</Text>
-      </View>
-    </View>
-  );
+	return (
+		<View style={styles.container}>
+			<View style={styles.albumInfo}>
+				<Image style={styles.albumImage} source={{ uri: image }} />
+				<View style={styles.text}>
+					<Text>{name}</Text>
+					<Text style={styles.artist}>{artist}</Text>
+				</View>
+			</View>
+			<RatingDropdown />
+		</View>
+	);
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flexDirection: "row",
-    padding: 10,
-  },
-  albumImage: {
-    marginRight: 10,
-    width: 50,
-    height: 50,
-  },
-  text: {
-    justifyContent: "center",
-  },
-  artist: {
-    color: "gray",
-    fontSize: 12,
-  },
+	container: {
+		flexDirection: "row",
+		padding: 10,
+		justifyContent: "space-between",
+		alignItems: "center",
+	},
+	albumInfo: {
+		flexDirection: "row",
+	},
+	albumImage: {
+		marginRight: 10,
+		width: 50,
+		height: 50,
+	},
+	text: {
+		justifyContent: "center",
+	},
+	artist: {
+		color: "gray",
+		fontSize: 12,
+	},
 });
 
 export default Album;
